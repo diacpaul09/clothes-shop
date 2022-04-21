9fa3cfbfb4d6d390a28b4b26fb8f12a0ee472e2d
diacpaul09 <diacpaul12@gmail.com>
2022-04-21 20:12:16 +0300

    aded

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
+import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HatsPage</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+     
+     <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
-      {/* <HomePage /> */}
     </div>
   );
 }
