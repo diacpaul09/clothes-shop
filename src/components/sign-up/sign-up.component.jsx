7cf64f26e5cd4a126fc209e6cd674f9ba3f8ba43
diacpaul09 <diacpaul12@gmail.com>
2022-04-28 14:09:57 +0300

    finishing sigin and signup methods

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { auth, createUserProfileDocumet, crecreateUserProfileDocumet } from "../../firebase/firebase.utils";
+import { auth, createUserProfileDocumet } from "../../firebase/firebase.utils";
 import './sign-up.styles.scss'
 
 
